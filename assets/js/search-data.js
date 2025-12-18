a67767d239642adca08367c2bc058a0feda4fc89
thekan0723 <thekan0723@gmail.com>
2025-12-18 04:00:09 +0000

    Deploying to gh-pages from @ kier-ai-cs/home@9663ed31d938053a29465cdd4eedb307d043ea16 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -26,9 +26,9 @@ ninja.data = [{
           description: "선임연구원 | 전산유체역학",
           section: "Members",handler: () => {
               window.location.href = "/home/members/sangwoo-kim/";
-            },},{id: "members-한글이름",
-          title: '한글이름',
-          description: "직급 | 파트",
+            },},{id: "members-김덕환",
+          title: '김덕환',
+          description: "책임연구원 | AI·데이터 과학",
           section: "Members",handler: () => {
               window.location.href = "/home/members/thekan/";
             },},{id: "members-이웅희",
